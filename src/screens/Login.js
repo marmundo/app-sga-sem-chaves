6d8a37e614161409f1b1c988cbca41e7c04b3bd9
Marcelo Damasceno de Melo <marcelo.damasceno@ifrn.edu.br>
2020-03-22 18:10:35 -0300

    adiciona a funcionalidade de fazer o login pelo suap

--- src/screens/Login.js
+++ src/screens/Login.js
@@ -1,21 +1,24 @@
 import React, { Component } from 'react'
+import api from '../core/services/api'
 // import { connect } from 'react-redux'
 // import { login } from '../store/actions/user'
+
 
 import {
     View,
     Text,
     StyleSheet,
     TouchableOpacity,
-    TextInput
+    TextInput,
+    Alert
 } from 'react-native'
 
 class Login extends Component {
     state = {
         name: 'Temporario',
-        email: '',
+        username: '',
         password: '',
-        error:'',
+        error: '',
     }
 
     componentDidUpdate = prevProps => {
@@ -25,51 +28,45 @@ class Login extends Component {
     }
 
     login = async () => {
-        if (this.state.email.length === 0 || this.state.password.length === 0) {
+        if (this.state.username.length === 0 || this.state.password.length === 0) {
             this.setState({ error: 'Preencha usuário e senha para continuar!' }, () => false);
-          } else {
-            //   Codigo de https://github.com/Rocketseat/blog-adonis-reactjs-react-native-airbnb-app/blob/master/src/pages/signIn/index.js
-        //     try {
-        //       const response = await api.post('/sessions', {
-        //         email: this.state.email,
-        //         password: this.state.password,
-        //       });
-      
-        //       await AsyncStorage.setItem('@AirBnbApp:token', response.data.token);
-      
-        //       const resetAction = StackActions.reset({
-        //         index: 0,
-        //         actions: [
-        //           NavigationActions.navigate({ routeName: 'Main' }),
-        //         ],
-        //       });
-        //       this.props.navigation.dispatch(resetAction);
-        //     } catch (_err) {
-        //     //   console.tron.log(_err);
-        //     console.log(_err)
-        //       this.setState({ error: 'Houve um problema com o login, verifique suas credenciais!' });
-        //     }
-          }
+        } else {
+            try {
+                //   Codigo de https://github.com/Rocketseat/blog-adonis-reactjs-react-native-airbnb-app/blob/master/src/pages/signIn/index.js
+                const response = await api.post('/', {
+                    username: this.state.username,
+                    password: this.state.password,
+                });
+                const userToken = response.data.token
+                console.log(userToken)
+                // await AsyncStorage.setItem('@AirBnbApp:token', response.data.token);
+
+                // TODO: Configurar o react navigation
+                // this.props.navigation.navigate('Sala');
+            } catch (_err) {
+                //console.tron.log(_err);
+                this.setState({ error: 'Houve um problema com o login, verifique suas credenciais!' });
+                Alert.alert(
+                    'Erro',
+                    this.state.error
+                )
+            }
+        }
     }
 
     render() {
         return (
             <View style={styles.container}>
-                <TextInput placeholder='Email' style={styles.input}
-                    autoFocus={true} keyboardType='email-address'
-                    value={this.state.email}
-                    onChangeText={email => this.setState({ email })} />
+                <TextInput placeholder='Matricula' style={styles.input}
+                    autoFocus={true} keyboardType='number-pad'
+                    value={this.state.username}
+                    onChangeText={username => this.setState({ username })} />
                 <TextInput placeholder='Senha' style={styles.input}
                     secureTextEntry={true} value={this.state.password}
                     onChangeText={password => this.setState({ password })} />
                 <TouchableOpacity onPress={this.login} style={styles.buttom}>
                     <Text style={styles.buttomText}>Login</Text>
                 </TouchableOpacity>
-                {/* <TouchableOpacity onPress={() => {
-                    this.props.navigation.navigate('Register')
-                }} style={styles.buttom}>
-                    <Text style={styles.buttomText}>Criar nova conta...</Text>
-                </TouchableOpacity> */}
             </View>
         )
     }
